3309871c0165bdbf0c6ad78d89ee8b24a9bf1286
Matt Grogan <mattgrogan.dev@gmail.com>
2022-01-20 17:53:40 -0700

    added functions on userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,8 @@ module.exports = {
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
       .select("-__v")
+      .populate("thought")
+      .populate("friend")
       .then((user) => {
         !user
           ? res.status(404).json({ message: "No user with that ID" })
@@ -22,6 +24,42 @@ module.exports = {
   },
 
   // POST a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
   // PUT to update a user by its _id
+
+  updateUser(req, res) {
+    User.findeOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
   // DELETE to remove user by its _id
+  deleteUser(req, res) {
+    User.findOneAndDelete({
+      _id: req.params.userId,
+    })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.status(200).json(res)
+      )
+      .then(() => res.json({ message: "User deleted!" }))
+      .catch((err) => res.status(500).json(err));
+  },
 };
